878819b343ebedffab0d0d91d5ef72058b1b8709
Sumit <sumit.tirole36@gmail.com>
2024-05-28 15:01:59 +0530

    Selenium version updated 4.21.0

--- src/main/java/com/flightfinder/pomrepo/CreateAlertElements.java
+++ src/main/java/com/flightfinder/pomrepo/CreateAlertElements.java
@@ -25,7 +25,7 @@ public class CreateAlertElements {
 		wait.until(ExpectedConditions.elementToBeClickable(element));	
 		wait.until(ExpectedConditions.presenceOfElementLocated(locator));
 		try {
-		Thread.sleep(1000); }
+		Thread.sleep(2000); }
 		catch (InterruptedException e) {
 		e.printStackTrace();	
 		}
