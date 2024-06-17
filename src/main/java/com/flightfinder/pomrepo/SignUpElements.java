e2d777f1c058daec21d75293d02ddf636f9205c4
Sumit <sumit.tirole36@gmail.com>
2024-06-17 20:49:46 +0530

    hit and trial

--- src/main/java/com/flightfinder/pomrepo/SignUpElements.java
+++ src/main/java/com/flightfinder/pomrepo/SignUpElements.java
@@ -22,7 +22,7 @@ public class SignUpElements {
 	
 	public void synchronised(By locator, WebElement element)  {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 		wait.until(ExpectedConditions.elementToBeClickable(element));	
 		wait.until(ExpectedConditions.presenceOfElementLocated(locator));
 		try {
